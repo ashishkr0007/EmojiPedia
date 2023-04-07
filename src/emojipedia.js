c7b6afc2a2e111939d1842168399fa672e609e10
Ashish Kumar <rajashish914@gmaail.com>
2023-04-08 01:47:17 +0530

    final commit for Emojipedia app

---
+++ src/emojipedia.js
@@ -0,0 +1,81 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "😍",
+    name: "Smiling Face with Heart-Eyes",
+    meaning:
+      "A yellow face with an open smile, sometimes showing teeth, and red, cartoon-styled hearts for eyes. Often conveys enthusiastic feelings of love, infatuation, and adoration, e.g., I love/am in love with this person or thing.",
+  },
+  {
+    id: 5,
+    emoji: "😋",
+    name: "Face Savoring Food",
+    meaning:
+      "A yellow face with smiling eyes and a broad, closed smile with its tongue",
+  },
+  {
+    id: 6,
+    emoji: "🤐",
+    name: "Zipper-Mouth Face",
+    meaning:
+      "A yellow face with simple, open eyes and a closed zipper for a mouth.",
+  },
+  {
+    id: 7,
+    emoji: "😎",
+    name: "Smiling Face with Sunglasses",
+    meaning:
+      "A yellow face with a broad, closed smile wearing black sunglasses",
+  },
+  {
+    id: 8,
+    emoji: "😵‍💫",
+    name: "Face with Spiral Eyes",
+    meaning:
+      "A face showing a spiral eyes, commonly used to indicate dizziness, but may also represent hypnosis or other forms of disorientation.",
+  },
+  {
+    id: 9,
+    emoji: "🤡",
+    name: "Clown Face",
+    meaning:
+      "A circus or birthday clown displayed with white face makeup, a red nose, exaggerated eyes and smile, and two tufts of hair (red or blue).",
+  },
+  {
+    id: 10,
+    emoji: "🥵",
+    name: "Hot Face",
+    meaning:
+      "A reddish-orange face with furrowed eyebrows, tongue stuck out, and beads of sweat, as if overheated from high temperatures",
+  },
+  {
+    id: 11,
+    emoji: "❤️",
+    name: " red love heart",
+    meaning:
+      "A classic red love heart emoji, used for expressions of love and romance.",
+  },
+];
+
+export default emojipedia;
